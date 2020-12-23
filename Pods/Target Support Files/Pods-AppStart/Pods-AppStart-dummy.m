a11ec930913a7599826ba6e692a4edbe2ce29966
Farid Ullah <fullah@blissapplications.com>
2020-12-23 11:34:52 +0000

    ignore pods

--- Pods/Target Support Files/Pods-AppStart/Pods-AppStart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AppStart : NSObject
-@end
-@implementation PodsDummy_Pods_AppStart
-@end
--- Pods/Target Support Files/Pods-SantanderTaskTests/Pods-SantanderTaskTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SantanderTaskTests : NSObject
-@end
-@implementation PodsDummy_Pods_SantanderTaskTests
-@end
